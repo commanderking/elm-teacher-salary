acfc4b1f078e253153f13dae5c2c96377ec3913f
commanderking <jek582@mail.harvard.edu>
2018-09-02 22:04:00 -0400

    make graphql request, but request not working yet

--- Main.elm
+++ Main.elm
@@ -7,7 +7,7 @@ import Json.Decode as Decode exposing(string, list)
 import Url.Builder as Url
 import Maybe
 main =
-  Browser.sandbox { init = init, update = update, view = view }
+  Browser.element { init = init, update = update, view = view, subscriptions = subscriptions }
 
 districts = [ {name = "Abington", code = "10000"}, { name = "Arlington", code = "20000"}]
 
@@ -15,20 +15,38 @@ districtOption district =
     option [ value district.code ] [ text district.name ]
 
 -- MODEL
-type alias Model = { incrementValue : Int, districtCode: String }
-init : Model
-init = { incrementValue = 0, districtCode = "" }
-
-
+type alias SalaryData = { name : String, averageSalary : String }
+type alias Model = { incrementValue : Int, districtCode: String, salaryData: List SalaryData }
+init : () -> (Model, Cmd Msg)
+init _ = ({ incrementValue = 0, districtCode = "", salaryData = [] }, Cmd.none)
+updateCode model code = { model | districtCode = code }
 -- UPDATE
 
-type Msg = UpdateDistrict String
+type Msg = UpdateDistrict String | ApplyDistrict | FetchTeacherSalary (Result Http.Error (List SalaryData))
 -- Takes two arguments, msg and model and returns model
-update : Msg -> Model -> Model
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     UpdateDistrict code ->
-      { model | districtCode = code }
+      (updateCode model code, Cmd.none)
+    ApplyDistrict ->
+      ( model, getTeacherSalary model.districtCode )
+    FetchTeacherSalary result ->
+      case result of
+        Ok newSalaryData ->
+          ( { model | salaryData = newSalaryData }
+          , Cmd.none
+          )
+
+        Err _ ->
+          ( model
+          , Cmd.none
+          )
+
+-- SUBSCRIPTIONS
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
 
 -- VIEW
 
@@ -38,10 +56,11 @@ view model =
     [ select [ onInput UpdateDistrict]
         (List.map districtOption (districts))
     , div [] [ text model.districtCode ]
+    , button [ onClick ApplyDistrict ] [ text "Apply" ]
     ]
         
 graphQLUrl : String
-graphQLUrl = "localhost:4000/graphql" 
+graphQLUrl = "http://localhost:4000/graphql"
 
 type alias TeacherSalary =
   { name : String
@@ -56,3 +75,8 @@ dataDecoder =
 
 postTeacherSalary : Http.Request (List TeacherSalary)
 postTeacherSalary = Http.post graphQLUrl Http.emptyBody (list dataDecoder)
+
+
+getTeacherSalary : String -> Cmd Msg
+getTeacherSalary code =
+  Http.send FetchTeacherSalary postTeacherSalary
